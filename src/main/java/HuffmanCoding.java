da718d745a4d2a725af3f34465370056b319b258
Binyamin <binyaminf22@gmail.com>
2022-04-05 15:43:46 -0500

    more documentation

--- src/main/java/HuffmanCoding.java
+++ src/main/java/HuffmanCoding.java
@@ -1,7 +1,5 @@
 import java.io.*;
-import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.List;
 import java.util.PriorityQueue;
 
 /**
@@ -16,187 +14,6 @@ import java.util.PriorityQueue;
  * the encoded data
  */
 public class HuffmanCoding {
-
-    /**
-     * The main method encodes/decodes files according to arguments
-     * @param args command line arguments
-     */
-    public static void main(String[] args) {
-        if (args.length != 3) {
-            System.out.println("Expected 3 arguments: mode <e or d>, input <filepath>, output <filepath>");
-        } else {
-            try {
-                // encode or decode depending on program arguments
-                File input = new File(args[1]);
-                File output = new File(args[2]);
-                if (args[0].toUpperCase().startsWith("E")) {
-                    encode(input, output);
-                    System.out.printf("Reduced file size by %f%%\n", 100 * (1 - (double) output.length() / input.length()));
-                } else {
-                    decode(input, output);
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    /**
-     * Compresses a file using Huffman coding
-     * @param inputFile handle to the input file
-     * @param outputFile handle to the output file
-     * @throws IOException if an error occurs during io operations
-     */
-    public static void encode(File inputFile, File outputFile) throws IOException {
-        // read file
-        InputStream input = new FileInputStream(inputFile);
-        // find frequencies
-        HuffmanTreeNode[] frequencies = new HuffmanTreeNode[256];
-        int next;
-        while ((next = input.read()) != -1) {
-            if (frequencies[next] == null) {
-                // create a new frequency node
-                HuffmanTreeNode node = new HuffmanTreeNode(0, false, next);
-                frequencies[next] = node;
-            }
-            frequencies[next].frequency++;
-        }
-        // create huffman tree using a priority queue
-        PriorityQueue<HuffmanTreeNode> queue = new PriorityQueue<>(Comparator.comparingInt(n -> n.frequency));
-        for (HuffmanTreeNode node : frequencies) {
-            if (node != null) {
-                queue.add(node);
-            }
-        }
-        if (queue.size() == 1) { // edge case
-            queue.add(new HuffmanTreeNode(0, true, -1));
-        }
-        while (queue.size() > 1) {
-            // join smallest two nodes into one internal node
-            HuffmanTreeNode min = queue.remove();
-            HuffmanTreeNode min2 = queue.remove();
-            HuffmanTreeNode internal = new HuffmanTreeNode(
-                    min.frequency + min2.frequency, true, -1
-            );
-            internal.left = min;
-            internal.right = min2;
-            queue.add(internal);
-        }
-        HuffmanTreeNode tree = queue.remove();
-        // calculate the huffman codes
-        String[] codes = new String[256];
-        findCodes(tree, codes, "");
-        // wrap output stream in my BitWriter class
-        OutputStream output = new FileOutputStream(outputFile);
-        BitWriter bits = new BitWriter(output);
-        // first two bytes start off blank because their info hasn't been calculated yet
-        bits.writeByte(0);
-        bits.writeByte(0);
-        // write each character and its corresponding huffman code to the file
-        int codeCount = 0;
-        for (int i = 0; i < codes.length; i++) {
-            String code = codes[i];
-            if (code == null) continue;
-            bits.writeByte(i);
-            bits.writeByte(code.length());
-            for (char character : code.toCharArray()) {
-                bits.writeBit(character - '0');
-            }
-            codeCount++;
-        }
-        // write the encoded data
-        input.close();
-        input = new FileInputStream(inputFile);
-        while ((next = input.read()) != -1) {
-            for (char character : codes[next].toCharArray()) {
-                bits.writeBit(character - '0');
-            }
-        }
-        int usedBits = bits.close();
-        // set the first two bytes
-        RandomAccessFile file = new RandomAccessFile(outputFile, "rw");
-        file.seek(0);
-        file.writeByte(usedBits);
-        file.seek(1);
-        file.writeByte(codeCount - 1);
-        file.close();
-    }
-
-    /**
-     * Decompresses a file that had been compressed with the encode method
-     * @param inputFile handle to the compressed input file
-     * @param outputFile handle to the output file
-     * @throws IOException if an error occurs during io operations
-     */
-    public static void decode(File inputFile, File outputFile) throws IOException {
-        // wrap an input stream in my BitReader class
-        InputStream input = new FileInputStream(inputFile);
-        BitReader bits = new BitReader(input);
-        // first two bytes
-        int usedBits = bits.nextByte();
-        int codeCount = bits.nextByte() + 1;
-        // recreate tree
-        HuffmanTreeNode tree = new HuffmanTreeNode(0, true, -1);
-        HuffmanTreeNode cursor;
-        for (int i = 0; i < codeCount; i++) {
-            int character = bits.nextByte();
-            int codeSize = bits.nextByte();
-            cursor = tree;
-            // follow each code, creating internal nodes as needed
-            for (int j = 0; j < codeSize; j++) {
-                int bit = bits.nextBit();
-                if (bit == 0) {
-                    if (cursor.left == null) {
-                        cursor.left = new HuffmanTreeNode(0, true, -1);
-                    }
-                    cursor = cursor.left;
-                } else {
-                    if (cursor.right == null) {
-                        cursor.right = new HuffmanTreeNode(0, true, -1);
-                    }
-                    cursor = cursor.right;
-                }
-            }
-            cursor.isInternal = false;
-            cursor.character = character;
-        }
-        // decode file
-        OutputStream output = new FileOutputStream(outputFile);
-        int lastBits = 0;
-        while (!(bits.isLastByte() && lastBits == usedBits)) {
-            cursor = tree;
-            while (cursor.isInternal) {
-                if (bits.nextBit() == 0) {
-                    cursor = cursor.left;
-                } else {
-                    cursor = cursor.right;
-                }
-                if (bits.isLastByte()) {
-                    lastBits++;
-                }
-            }
-            output.write(cursor.character);
-        }
-        bits.close();
-        output.close();
-    }
-
-
-    /**
-     * Recursive function that finds each code in a tree
-     * @param node the current node
-     * @param codes an array of Huffman codes for each possible byte
-     * @param path the Huffman code that would arrive at the current node
-     */
-    public static void findCodes(HuffmanTreeNode node, String[] codes, String path) {
-        if (node == null) return;
-        if (!node.isInternal) {
-            codes[node.character] = path;
-        } else {
-            findCodes(node.left, codes, path + "0");
-            findCodes(node.right, codes, path + "1");
-        }
-    }
 
     /** A simple class to store node info */
     public static class HuffmanTreeNode {
@@ -309,12 +126,7 @@ public class HuffmanCoding {
             // reads bit by bit
             int b = 0;
             for (int i = 0; i < 8; i++) {
-                b <<= 1;
-                if (nextBit() == 1) {
-                    b |= 1;
-                } else {
-                    b &= ~1;
-                }
+                b = b * 2 + nextBit();
             }
             return b;
         }
@@ -351,6 +163,212 @@ public class HuffmanCoding {
          */
         public void close() throws IOException {
             input.close();
+        }
+    }
+
+    /**
+     * The main method encodes/decodes files according to arguments
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+        if (args.length != 3) {
+            System.out.println("Expected 3 arguments: mode <e or d>, " +
+                    "input <filepath>, output <filepath>");
+        } else {
+            try {
+                // encode or decode depending on program arguments
+                File input = new File(args[1]);
+                File output = new File(args[2]);
+                if (args[0].toUpperCase().startsWith("E")) {
+                    encode(input, output);
+                    System.out.printf("Reduced file size by %f%%\n",
+                            100 * (1 - (double) output.length() / input.length()));
+                } else {
+                    decode(input, output);
+                }
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * Compresses a file using Huffman coding
+     * @param inputFile handle to the input file
+     * @param outputFile handle to the output file
+     * @throws IOException if an error occurs during io operations
+     */
+    public static void encode(File inputFile, File outputFile) throws IOException {
+        // read file
+        InputStream input = new FileInputStream(inputFile);
+        // find frequencies
+        HuffmanTreeNode[] frequencies = new HuffmanTreeNode[256];
+        int next;
+        while ((next = input.read()) != -1) {
+            if (frequencies[next] == null) {
+                // create a new frequency node
+                HuffmanTreeNode node = new HuffmanTreeNode(0, false, next);
+                frequencies[next] = node;
+            }
+            frequencies[next].frequency++;
+        }
+        // create tree
+        HuffmanTreeNode tree = createHuffmanTreeFromFrequencies(frequencies);
+        // calculate huffman codes
+        String[] codes = new String[256];
+        findCodes(tree, codes, "");
+        // wrap output stream in a BitWriter
+        OutputStream output = new FileOutputStream(outputFile);
+        BitWriter bits = new BitWriter(output);
+        // beginning of file can't be calculated until later
+        bits.writeByte(0);
+        bits.writeByte(0);
+        // write each character and its corresponding Huffman code to the file,
+        // see the top of this class for details of file structure
+        int codeCount = 0;
+        for (int i = 0; i < codes.length; i++) {
+            String code = codes[i];
+            if (code == null) continue;
+            bits.writeByte(i);
+            bits.writeByte(code.length());
+            for (char character : code.toCharArray()) {
+                bits.writeBit(character - '0');
+            }
+            codeCount++;
+        }
+        // write the encoded data
+        input.close();
+        input = new FileInputStream(inputFile);
+        while ((next = input.read()) != -1) {
+            for (char character : codes[next].toCharArray()) {
+                bits.writeBit(character - '0');
+            }
+        }
+        int usedBits = bits.close();
+        // set the first two bytes
+        RandomAccessFile file = new RandomAccessFile(outputFile, "rw");
+        file.seek(0); // first byte
+        file.writeByte(usedBits);
+        file.seek(1); // second byte
+        file.writeByte(codeCount - 1);
+        file.close();
+    }
+
+    /**
+     * Decompresses a file that had been compressed with the encode method
+     * @param inputFile handle to the compressed input file
+     * @param outputFile handle to the output file
+     * @throws IOException if an error occurs during io operations
+     */
+    public static void decode(File inputFile, File outputFile) throws IOException {
+        // wrap an input stream in a BitReader
+        InputStream input = new FileInputStream(inputFile);
+        BitReader bits = new BitReader(input);
+        // fetch first two bytes
+        int usedBits = bits.nextByte();
+        int codeCount = bits.nextByte() + 1;
+        // recreate tree
+        HuffmanTreeNode tree = createHuffmanTreeFromFile(bits, codeCount);
+        // decode file
+        OutputStream output = new FileOutputStream(outputFile);
+        int lastBits = 0;
+        HuffmanTreeNode cursor;
+        while (!(bits.isLastByte() && lastBits == usedBits)) {
+            cursor = tree;
+            while (cursor.isInternal) {
+                if (bits.nextBit() == 0) {
+                    cursor = cursor.left;
+                } else {
+                    cursor = cursor.right;
+                }
+                if (bits.isLastByte()) {
+                    lastBits++;
+                }
+            }
+            output.write(cursor.character);
+        }
+        bits.close();
+        output.close();
+    }
+
+    /**
+     * creates a huffman tree using a priority queue and a list of frequencies
+     * @param frequencies list of frequencies
+     * @return root of huffman tree
+     */
+    public static HuffmanTreeNode createHuffmanTreeFromFrequencies(HuffmanTreeNode[] frequencies) {
+        PriorityQueue<HuffmanTreeNode> queue = new PriorityQueue<>(Comparator.comparingInt(n -> n.frequency));
+        for (HuffmanTreeNode node : frequencies) {
+            if (node != null) {
+                queue.add(node);
+            }
+        }
+        if (queue.size() == 1) { // edge case
+            queue.add(new HuffmanTreeNode(0, true, -1));
+        }
+        while (queue.size() > 1) {
+            // join smallest two nodes into one internal node
+            HuffmanTreeNode min = queue.remove();
+            HuffmanTreeNode min2 = queue.remove();
+            HuffmanTreeNode internal = new HuffmanTreeNode(
+                    min.frequency + min2.frequency, true, -1
+            );
+            internal.left = min;
+            internal.right = min2;
+            queue.add(internal);
+        }
+        return queue.remove();
+    }
+
+    /**
+     * creates a Huffman tree from a file which is already reading at the proper location
+     * see top of this class for file structure details
+     * @param bits a BitReader to read from
+     * @param codeCount the number of Huffman codes stored in the file
+     * @return a Huffman tree
+     * @throws IOException if something goes wrong while reading
+     */
+    public static HuffmanTreeNode createHuffmanTreeFromFile(BitReader bits, int codeCount) throws IOException {
+        HuffmanTreeNode tree = new HuffmanTreeNode(0, true, -1);
+        HuffmanTreeNode cursor;
+        for (int i = 0; i < codeCount; i++) {
+            int character = bits.nextByte();
+            int codeSize = bits.nextByte();
+            cursor = tree;
+            // follow each code, creating internal nodes as needed
+            for (int j = 0; j < codeSize; j++) {
+                int bit = bits.nextBit();
+                if (bit == 0) {
+                    if (cursor.left == null) {
+                        cursor.left = new HuffmanTreeNode(0, true, -1);
+                    }
+                    cursor = cursor.left;
+                } else {
+                    if (cursor.right == null) {
+                        cursor.right = new HuffmanTreeNode(0, true, -1);
+                    }
+                    cursor = cursor.right;
+                }
+            }
+            cursor.isInternal = false;
+            cursor.character = character;
+        }
+        return tree;
+    }
+
+    /**
+     * Recursive function that finds each code in a tree
+     * @param node the current node
+     * @param codes an array of Huffman codes for each possible byte
+     * @param path the Huffman code that would arrive at the current node
+     */
+    public static void findCodes(HuffmanTreeNode node, String[] codes, String path) {
+        if (node == null) return;
+        if (!node.isInternal) {
+            codes[node.character] = path;
+        } else {
+            findCodes(node.left, codes, path + "0");
+            findCodes(node.right, codes, path + "1");
         }
     }
 }
